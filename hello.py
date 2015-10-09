dbc6bd92931c267005c133dbb47b3173808cc03d
Alex Altair <alexanderaltair@gmail.com>
2015-10-09 16:57:01 -0700

    Import sklearn.

--- hello.py
+++ hello.py
@@ -1,8 +1,14 @@
 import os
 from flask import Flask
 
+from sklearn import datasets
+
 app = Flask(__name__)
+app.config['DEBUG'] = True
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return str(datasets.load_iris().data)
+
+if __name__ == "__main__":
+    app.run()
