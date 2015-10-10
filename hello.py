e9f0d5ac94627c8c42a6551fcd410975702112bd
Alex Altair <alexanderaltair@gmail.com>
2015-10-10 03:44:06 -0700

    Create file upload and submit form.

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 from flask_bootstrap import Bootstrap
 from flask.ext import uploads
@@ -15,6 +15,11 @@ Bootstrap(app)
 def hello():
     iris = datasets.load_iris().data
     return render_template('index.html', data=iris)
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    data = request.files['file'].read()
+    return render_template('index.html', data=data)
 
 if __name__ == "__main__":
     app.run()
