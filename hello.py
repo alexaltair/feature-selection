468ba6450de9efe4c83ef417eb9089f76546d227
Alex Altair <alexanderaltair@gmail.com>
2015-10-10 02:58:22 -0700

    Add Bootstrap and first template.

--- hello.py
+++ hello.py
@@ -1,14 +1,20 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
+
+from flask_bootstrap import Bootstrap
+from flask.ext import uploads
 
 from sklearn import datasets
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+Bootstrap(app)
+
 @app.route('/')
 def hello():
-    return str(datasets.load_iris().data)
+    iris = datasets.load_iris().data
+    return render_template('index.html', data=iris)
 
 if __name__ == "__main__":
     app.run()
