5f4112e1b25e05c76867bfa108283f6f169e1b67
Alex Altair <alexanderaltair@gmail.com>
2015-10-24 16:04:00 -0700

    Test redis on Heroku.

--- routes.py
+++ routes.py
@@ -1,11 +1,17 @@
 from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
+from cStringIO import StringIO
+import cPickle as pickle
+import os, redis
 import pandas
 from stats import generate_results
 
 app = Flask(__name__)
 
 Bootstrap(app)
+
+# r = redis.from_url(os.environ.get("REDIS_URL"))
+r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 @app.route('/')
 def hello():
@@ -20,8 +26,19 @@ def upload():
     else:
         data = pandas.read_csv(data_file, header=None)
 
-    results = generate_results(data)
 
+    mystring = StringIO()
+    pickle.dump(data, mystring)
+    mystring.seek(0)
+    r.set('mykey', mystring.read())
+    mystring.close()
+
+    data = StringIO(r.get('mykey'))
+    r.delete('mykey')
+    data = pickle.load(data)
+
+
+    results = generate_results(data)
     return render_template('index.html', **results)
 
 if __name__ == "__main__":
